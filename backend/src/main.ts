d12ed5fbb7ead392ed8470fc0b42a83c28c80136
yg1110 <younggil9488@gmail.com>
2026-01-04 23:43:02 +0900

    fix: cors

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -5,16 +5,7 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.enableCors({
-    origin: [
-      'http://localhost:3000',
-      'http://34.64.73.115:3000',
-    ],
-    credentials: true,
-    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization'],
-  });
-
+  app.enableCors();
 
   const config = new DocumentBuilder()
     .setTitle('RMancer API 문서')
